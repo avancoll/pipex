b6598f874c1c21e2c7d37ddfb4a9d1ac2e6bb94b
Antoine Vancollie <113617810+avancoll@users.noreply.github.com>
2023-01-05 21:48:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -15,5 +15,25 @@
 
 int	main(int argc, char **argv, char **env)
 {
+	int		fd[2];
+	pid_t	pid;
+
+	if (pipe(fd) == -1)
+		return (1);
+  pid = fork();
+	if (pid == -1)
+    return (2);
+	if (!pid)
+		child_process(argv, env, fd);
+	else
+		main_process(argv, env, fd);
+	waitpid(pid, 0, 0);
+	close(fd[0]);
+	close(fd[1]);
+	return (0);
+}
+
+int	main(int argc, char **argv, char **env)
+{
 	execve(argv[1], argv + 1, env);
 }
