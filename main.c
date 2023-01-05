c5ce92722d096fa4e37783de122053d9cafe1ff8
Antoine Vancollie <113617810+avancoll@users.noreply.github.com>
2023-01-05 22:11:24 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,6 +13,32 @@
 #include "pipex.h"
 #include <stdio.h>
 
+void	child_process(char **argv, char **env, int *fd)
+{
+	int	inputfd;
+
+	inputfd = open(argv[1], O_RDONLY, 0777);
+	if (inputfd == -1)
+		return (1);
+	dup2(fd[1], STDOUT_FILENO);
+	dup2(inputfd, STDIN_FILENO);
+	close(fd[0]);
+	exec(argv[2], env);
+}
+
+void	main_process(char **argv, char **env, int *fd)
+{
+	int		outputfd;
+
+	outputfd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (outputfd == -1)
+		error();
+	dup2(fd[0], STDIN_FILENO);
+	dup2(outputfd, STDOUT_FILENO);
+	close(fd[1]);
+	exec(argv[3], env);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	int		fd[2];
@@ -31,9 +57,4 @@ int	main(int argc, char **argv, char **env)
 	close(fd[0]);
 	close(fd[1]);
 	return (0);
-}
-
-int	main(int argc, char **argv, char **env)
-{
-	execve(argv[1], argv + 1, env);
 }
