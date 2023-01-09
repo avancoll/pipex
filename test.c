53aeb1a40b0ce8bead8b77a36b60bf95c5622a63
Antoine Vancollie <avancoll@fu-r9-p8.s19.be>
2023-01-09 18:46:28 +0100

    more tests

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: avancoll <avancoll@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:24:37 by avancoll          #+#    #+#             */
-/*   Updated: 2023/01/09 17:27:57 by avancoll         ###   ########.fr       */
+/*   Updated: 2023/01/09 18:44:35 by avancoll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*res;
 	size_t	i;
@@ -59,39 +59,36 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!res)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
+	i = -1;
+	while (s1[++i])
 		res[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
+	free(s1);
+	j = -1;
+	while (s2[++j])
 		res[i + j] = s2[j];
-		j++;
-	}
 	res[i + j] = '\0';
 	return (res);
 }
 
-
-
 int	main(int argc, char **argv, char **env)
 {
-	int	i = 0;
+	int		i;
 	char	**path;
+	char	**cmd;
+
+	i = 0;
 	while (!ft_strnstr(env[i], "PATH=", 5))
 		i++;
 	path = ft_split(env[i] + 5, ':');
-	i = 0;
-	while (path[i])
+	cmd = ft_split(argv[2], ' ');
+	i = -1;
+	while (path[++i])
 	{
-		path[i] = ft_strjoin(path[i], "/ls");
-		i++;
+		path[i] = ft_strjoin(path[i], "/");
+		path[i] = ft_strjoin(path[i], cmd[0]);
 	}
 	i = 0;
 	while (access(path[i], 0))
 		i++;
-	printf("%s\n", path[i]);
+	execve(path[i], cmd, env);
 }
